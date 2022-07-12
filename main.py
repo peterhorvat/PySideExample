9b5579f105944941a9fbf4750b5f56dc0c3bb9cd
peter <peter.horvat@cadis.si>
2022-07-12 12:37:13 +0200

    Example GUI

--- main.py
+++ main.py
@@ -1,32 +1,121 @@
 import sys
-from PySide2.QtWidgets import (QLineEdit, QPushButton, QApplication,
-    QVBoxLayout, QDialog)
 
-class Form(QDialog):
+from PySide2.QtCore import QSize
+from PySide2.QtWidgets import QApplication, QVBoxLayout, QWidget, QPushButton, QListWidget, QHBoxLayout, QLabel, QComboBox, QListWidgetItem
+import qtawesome as qta
 
-    def __init__(self, parent=None):
-        super(Form, self).__init__(parent)
-        # Create widgets
-        self.edit = QLineEdit("Write my name here")
-        self.button = QPushButton("Show Greetings")
-        # Create layout and add widgets
-        layout = QVBoxLayout()
-        layout.addWidget(self.edit)
-        layout.addWidget(self.button)
-        # Set dialog layout
+
+class DropDownList(QWidget):
+    def __init__(self, parent=None, item=None):
+        super(DropDownList, self).__init__(parent)
+
+        self.item = item
+        layout = QHBoxLayout()
+        self.cb = QComboBox()
+        self.cb.addItem(f"No {self.item}")
+
+        layout.addWidget(self.cb)
         self.setLayout(layout)
-        # Add button signal to greetings slot
-        self.button.clicked.connect(self.greetings)
 
-    # Greets the user
-    def greetings(self):
-        print ("Hello %s" % self.edit.text())
+    def addElement(self):
+        self.cb.addItem(f"{self.item} {self.cb.count()}")
+        self.cb.setCurrentIndex(self.cb.count()-1)
+
+
+class HorizontalElements(QWidget):
+    def __init__(self, parent=None, item=None):
+        super(HorizontalElements, self).__init__(parent)
+
+        self.dlist = DropDownList(item=item)
+
+        self.button = QPushButton(self)
+        self.button.setIcon(qta.icon("msc.add"))
+        self.button.clicked.connect(self.dlist.addElement)
+        self.button.setFixedSize(self.button.iconSize().width() + 5, self.button.iconSize().height() + 5)
+
+        self.window = QWidget()
+        self.layout = QHBoxLayout()
+
+        self.label = QLabel(self)
+        self.label.setText(f"{item}:")
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.dlist)
+        self.layout.addWidget(self.button)
+        self.window.setLayout(self.layout)
+
+
+class ListBox(QWidget):
+    def __init__(self, parent=None, newItem=None, itemName=None, extension=None, icon=None):
+        super(ListBox, self).__init__(parent)
+
+        self.newItem = newItem
+        self.itemName = itemName
+        self.extension = extension
+        self.icon = icon
+
+        self.widget = QListWidget()
+        self.layout = QVBoxLayout(self)
+        self.button = QPushButton(f"{newItem}", self)
+        self.button.clicked.connect(self.onClicked)
+        self.layout.addWidget(self.widget)
+        self.layout.addWidget(self.button)
+
+    def onClicked(self):
+        item = QListWidgetItem()
+        item.setText(f"{self.itemName}_{self.widget.count()}{self.extension if self.extension else ''}")
+        if self.icon:
+            item.setIcon(qta.icon(self.icon))
+        self.widget.addItem(item)
+
+
+class LeftSide(QWidget):
+    def __init__(self, parent=None):
+        super(LeftSide, self).__init__(parent)
+
+        self.window = QWidget()
+        self.layout = QVBoxLayout()
+
+        self.projects = HorizontalElements(item="Project")
+        self.categories = HorizontalElements(item="Category")
+        self.tasks = ListBox(newItem="New task", itemName="task", icon="fa.file-image-o")
+
+        self.layout.addWidget(self.projects.window)
+        self.layout.addWidget(self.categories.window)
+        self.layout.addWidget(self.tasks)
+        self.window.setLayout(self.layout)
+
+
+class RightSide(QWidget):
+    def __init__(self, parent=None):
+        super(RightSide, self).__init__(parent)
+
+        self.window = QWidget()
+        self.layout = QVBoxLayout()
+
+        self.files = ListBox(newItem="Publish file", itemName="file1", extension=".ma", icon="fa.file-text")
+        self.layout.addWidget(self.files)
+        self.window.setLayout(self.layout)
+
+
+class Window(QWidget):
+
+    def __init__(self, title=None):
+        super().__init__()
+
+        layout = QHBoxLayout()
+        layout.addWidget(LeftSide().window)
+        layout.addWidget(RightSide().window)
+
+        self.window().setWindowTitle(title)
+        self.window().setLayout(layout)
+
+        self.setGeometry(300, 300, 660, 660)
+
+        self.show()
+
 
 if __name__ == '__main__':
-    # Create the Qt Application
     app = QApplication(sys.argv)
-    # Create and show the form
-    form = Form()
+    form = Window(title="Maya AssetManager")
     form.show()
-    # Run the main Qt loop
     sys.exit(app.exec_())
